7f7515504a1846a639329c8a5faebcb3bed8787c
Karl Parkinson <karl.p@getjobber.com>
2016-09-03 17:21:08 -0600

    validation token testing

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 'use strict';
 
 const express = require('express');
-
+const https = require('https');
 
 var app = express();
+app.set('port', process.env.PORT || 5000);
+
+const VALIDATION_TOKEN = process.env.VALIDATION_TOKEN;
+
+if (!VALIDATION_TOKEN) {
+  console.error("VALIDATION_TOKEN missing");
+  process.exit(1);
+}
 
 app.get("/", function(req, res){
-  res.send("Hello");
+  res.send(VALIDATION_TOKEN)
 })
 
-app.set('port', process.env.PORT || 5000);
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
