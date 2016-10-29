23e5742073c9afd0047ea562497cb8bdb770ee14
Karl Parkinson <karl.p@getjobber.com>
2016-10-29 14:57:11 -0600

    request verification and parsing request

--- app.js
+++ app.js
@@ -75,15 +75,42 @@ function respondToTextMessage(sendTo, msg) {
   callSendAPI(messageData);
 }
 
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+  
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+    
+    var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+        .update(buf)
+        .digest('hex');
+    
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
+
+
 const express = require('express');
 const https = require('https');
 const request = require('request');
+const crypto = require('crypto');
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
+app.use(bodyParse.json({ verify: verifyRequestSignature }));
+app.use(express.static('public'));
 
 const VALIDATION_TOKEN = process.env.VALIDATION_TOKEN;
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const APP_SECRET = process.env.APP_SECRET;
 
 if (!VALIDATION_TOKEN) {
   console.error("VALIDATION_TOKEN missing");
